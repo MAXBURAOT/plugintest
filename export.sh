1eb3b63fa0aa726a205456a9cef806012224ae70
Andrei Karas <akaras@inbox.ru>
2015-08-09 20:29:19 +0300

    Add into export script some more symbols.

--- export.sh
+++ export.sh
@@ -10,8 +10,8 @@ case `uname` in
 esac
 
 echo "main exports"
-nm "main"|grep "foo"
+nm "main"|egrep "foo|HPM_shared_symbols|import_symbol"
 echo "plugin1 exports"
-nm "plugin1.$PLUGINEXT"|grep "foo"
+nm "plugin1.$PLUGINEXT"|egrep "foo|HPM_shared_symbols|import_symbol"
 echo "plugin2 exports"
-nm "plugin2.$PLUGINEXT"|grep "foo"
+nm "plugin2.$PLUGINEXT"|egrep "foo|HPM_shared_symbols|import_symbol"
